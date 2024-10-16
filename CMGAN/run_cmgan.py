44ad51d9b141025e228438df5b1788e4eb1999b4
hong.jincheng <hong.jincheng@hellogroup.com>
2024-10-16 18:28:59 +0800

    debug

--- CMGAN/run_cmgan.py
+++ CMGAN/run_cmgan.py
@@ -1,5 +1,6 @@
 from recbole.quick_start import run_recbole
-
+import pdb
+pdb.set_trace()
 config_file_list = ['CMGAN-ml.yaml']
 run_recbole(model='CMGAN', dataset='ml-1m', config_file_list=config_file_list)
 
